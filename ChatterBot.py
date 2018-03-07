7e02aa663e9dbf02617b9b1bc12e297afd5226e1
unknown <saranyababuselvan@gmail.com>
2018-03-06 20:59:31 -0800

    heroku

--- ChatterBot.py
+++ ChatterBot.py
@@ -4,10 +4,10 @@ import json
 import time
 
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+consumer_key = "H9xp5a91XNCTVTozToBH5Cnxc"
+consumer_secret = "av6KrAgP1VfBEVxtdwO2rJppHE4nad1DcPNbqHCmBXlnrR6PnZ"
+access_token = "969399719125336064-BjEeNcwLmGFeDCXqhkhULzFbj48zG2x"
+access_token_secret = "wlCzvVtSLHgZdrjQzNBhmNEWCQxbydoxVOuyASlz5v1R8" 
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
